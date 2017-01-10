250c61ca7d8e38a894cb733f3a818cda606d3be5
yosvanilopez2 <yosvanilopez@nat-oitwireless-inside-vapornet100-d-10171.Princeton.EDU>
2017-01-10 01:52:38 -0500

    changed to matt

--- CASLoginVC.swift
+++ CASLoginVC.swift
@@ -50,14 +50,14 @@ class CASLoginVC: UIViewController, UIWebViewDelegate{
             let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
             
             if let netid = dataString?.components(separatedBy: "\n")[1]{
-                self.retreiveUserInfo(netid: "pvasquez")
+                self.retreiveUserInfo(netid: "mja3")
             }
         }
         task.resume()
     }
     
     func retreiveUserInfo(netid: String) {
-        let email = "pvasquez@princeton.edu"
+        let email = "mja3@princeton.edu"
         // work out persistence login logic
         attemptLogIn(email: email) { (success, data) in
             if success {
